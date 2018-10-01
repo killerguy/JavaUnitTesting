14bc8ea32a8e69aa433520bbe5b0ef4f952640d4
mc60991 <chaudharimukul@johndeere.com>
2018-10-01 18:01:29 +0530

    Added Test for DB Connection

---
+++ src/main/java/com/mukul/connection/DBConnection.java
@@ -0,0 +1,18 @@
+package com.mukul.connection;
+
+        import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.SQLException;
+
+public class DBConnection {
+    private Connection dbConnection;
+
+    public void getDBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:6666/mukul", "root", "password");
+    }
+
+    public int executeQuery(String query) throws ClassNotFoundException, SQLException {
+        return dbConnection.createStatement().executeUpdate(query);
+    }
+}
